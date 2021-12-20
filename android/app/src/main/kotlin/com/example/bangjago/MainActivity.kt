adacf54f4b39f9d1afa6a8c0a3bd8acf5f0539b1
rizqidoet <muhamadrizqifadila@gmail.com>
2021-12-20 22:16:29 +0700

    first Commit

---
+++ android/app/src/main/kotlin/com/example/bangjago/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bangjago
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = Color(0xFFFFFFFF);
+Color blackColor = Color(0xFF000000);
+Color orangeColor = Color(0xFFff8600);
+Color grayColor = Color(0xFF5d5d5d);
+Color semiWhiteColor = Color(0xFFf0f0f0);
